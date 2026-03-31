dc2d949ce8d19af3485fcbabcf210983bbf58da2
Rianless <95962850+Rianless@users.noreply.github.com>
2026-04-01 02:51:27 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,3 @@
-const CACHE = 'horang2-v1';
-const CACHE_URLS = ['/'];
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CACHE_URLS).catch(()=>{})));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-
-self.addEventListener('fetch', e => {
-  // API 요청은 캐시 안 함
-  if (e.request.url.includes('/api/')) return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
